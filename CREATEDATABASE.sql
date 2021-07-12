262edea07685d580b8704ce96b2ed144f116e34f
DavidBarbosa425 <davidbarbosa425@gmail.com>
2021-07-12 13:55:33 -0300

    Initial commit

---
+++ CREATEDATABASE.sql
@@ -0,0 +1,16 @@
+create database cadastro
+default character set utf8
+default collate utf8_general_ci;
+
+use cadastro;
+
+CREATE TABLE pessoas (
+    id INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(30) NOT NULL,
+    nascimento DATE,
+    sexo ENUM('M', 'F'),
+    peso DECIMAL(5 , 2 ),
+    altura DECIMAL(3 , 2 ),
+    nacionalidade VARCHAR(20) DEFAULT 'Brasil',
+    primary key (id)
+)  DEFAULT CHARSET=UTF8;
---
+++ README.md
@@ -0,0 +1,2 @@
+# BancoDeDadosMySQLCV
+ 
