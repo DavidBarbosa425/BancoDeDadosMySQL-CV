33577a41cb71710fafbe9cb25745f68c3ac9b056
DavidBarbosa425 <davidbarbosa425@gmail.com>
2021-07-17 23:41:55 -0300

    alter table

--- Aula01/TablePessoas.sql
+++ Aula01/TablePessoas.sql
@@ -1,8 +1,12 @@
-insert into pessoas
-(nome, nascimento, sexo, peso, altura, nacionalidade)
-values
-('Godofredo', '1984-01-02', 'M', '78.5', '1.83', 'Brasil'),
-('Ellen', '1988-04-04', 'F', '65.5', '1.60', 'Brasil'),
-('David', '1987-06-30', 'M', '97.5', '1.80', 'Brasil');
- 
-select * from pessoas;
+create table if not exists cursos (
+nome varchar(20) not null unique,
+descricao text,
+carga int,
+totaulas int,
+ano year default '2016'
+)default charset = utf8;
+
+alter table cursos
+change column idcursos idcurso int not null;
+
+describe cursos
--- Aula02/InsertInto.sql
+++
@@ -1,9 +0,0 @@
-use cadastro;
-insert into pessoas
-(nome, nascimento, sexo, peso, altura, nacionalidade)
-values
-('Godofredo', '1984-01-02', 'M', '78.5', '1.83', 'Brasil'),
-('Ellen', '1988-04-04', 'F', '65.5', '1.60', 'Brasil'),
-('David', '1987-06-30', 'M', '97.5', '1.80', 'Brasil');
- 
-select * from pessoas;
