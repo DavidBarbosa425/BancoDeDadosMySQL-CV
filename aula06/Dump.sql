75bb01a3e88f88fd4283b1cd860434fda9ba48d9
DavidBarbosa425 <davidbarbosa425@gmail.com>
2021-07-19 18:21:34 -0300

    Dump, data export e import
    Server, data import ou export...

---
+++ aula06/Dump.sql
@@ -0,0 +1,21 @@
+use cadastro;
+
+select * from gafanhotos;
+
+select * from cursos;
+
+describe gafanhotos;
+
+describe cursos;
+
+drop database cadastro;
+
+use cadastro;
+
+select * from gafanhotos;
+
+select * from cursos;
+
+describe gafanhotos;
+
+describe cursos;
