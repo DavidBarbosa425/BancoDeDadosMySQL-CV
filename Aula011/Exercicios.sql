c4358fc4b6fb605b6f53d1843a4befe48b719315
DavidBarbosa425 <davidbarbosa425@gmail.com>
2021-07-21 15:17:14 -0300

    Exercícios

---
+++ Aula011/Exercicios.sql
@@ -0,0 +1,19 @@
+use cadastro;
+
+select profissao, count(*) from gafanhotos
+group by profissao
+order by profissao;
+
+select sexo, count(*) from gafanhotos
+where nascimento > '2005-01-01'
+group by sexo;
+
+select nacionalidade, count(*) from gafanhotos
+where nacionalidade != 'Brasil'
+group by nacionalidade
+having count(*) > 3;
+
+select altura, peso, count(*) from gafanhotos
+where peso > '100'
+group by altura
+having altura > (select avg(altura) from gafanhotos);
